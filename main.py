6693a88f6aabc91d22f2b93a33a583f723d74392
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2024-12-04 18:16:43 +0200

    i mean it's alright

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QCalendarWidget
 from PyQt6 import uic
 import sys
 from PyQt6.QtWidgets import QDialog, QListWidget, QListWidgetItem
+from db import db_work
+from config import db_name
 
 
 class Dialog_taskmaker(QDialog):
@@ -15,6 +17,9 @@ class Dialog_taskmaker(QDialog):
         self.year_lineEdit.setText(str(dates[0]))
         self.month_lineEdit.setText(str(dates[1]))
         self.day_lineEdit.setText(str(dates[2]))
+        self.task_name_lineEdit.setText("Your task")
+
+        self.db_workk = db_work(db_name)
 
     def closing(self):
         self.close()
@@ -22,13 +27,26 @@ class Dialog_taskmaker(QDialog):
     def commiting(self):
         print(self.day_lineEdit.text())
         print(self.timeEdit.time().hour(), self.timeEdit.time().minute())
-
+        times = f"{self.timeEdit.time().hour()}:{self.timeEdit.time().minute()}"
+        self.db_workk = db_work(db_name)
+        dates = f"{self.day_lineEdit.text()}.{self.month_lineEdit.text()}.{self.year_lineEdit.text()}"
+        labell = self.task_name_lineEdit.text()
+        self.db_workk.insert_task(dates, times, labell)
+        self.close()
 
 class tasks_list_Dialog(QDialog):
     def __init__(self, parent_class, dates=[]):
         super().__init__()
         uic.loadUi('UI_files/tasks_list_dialog.ui', self)
-
+        
+        self.db_workk = db_work(db_name)
+        quers = self.db_workk.get_tasks()
+        #print(quers)
+        for row in quers:
+            # print(row)
+            values = "   ".join(map(str, row))
+            # print("___".join(row))
+            self.tasksList_listWidget.addItem(values)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -40,11 +58,11 @@ class MainWindow(QMainWindow):
         self.button_showList.clicked.connect(self.open_tasks_list)
 
     def adding_task(self):
-        print(52)
+        # print(52)
         date_object = self.calendarWidget.selectedDate()
         # print(self.calendarWidget.selectedDate().toString()) # toStdSysDays()
         # print(self.calendarWidget.selectedDate().day())
-        print(date_object.year(), date_object.month(), date_object.day())
+        # print(date_object.year(), date_object.month(), date_object.day())
         dil = Dialog_taskmaker(self, [date_object.year(), date_object.month(), date_object.day()])
         dil.exec()
         # self.label.setText("OK")
@@ -54,12 +72,38 @@ class MainWindow(QMainWindow):
         dates = [date_object.year(), date_object.month(), date_object.day()]
         self.chosenDay_lineEdit.setText(f"{dates[2]}.{dates[1]}.{dates[0]}")
 
+        self.db_workk = db_work(db_name)
+        quers = self.db_workk.get_day_tasks(f"{dates[2]}.{dates[1]}.{dates[0]}")
+        #print(quers)
+        self.listWidget.clear()
+        for row in quers:
+            # print(row)
+            values = "   ".join(map(str, row))
+            # print("___".join(row))
+            self.listWidget.addItem(values)
+
     def deleting(self):
-        print("deleting")
+        # print("deleting")
+
         # print(self.listWidget.currentItem().text())
+        # print(self.listWidget.currentItem().text().split()[0])
+        if self.listWidget.currentItem().text().split()[0]:
+            id = self.listWidget.currentItem().text().split()[0]
+            self.db_workk = db_work(db_name)
+        # dates = f"{self.day_lineEdit.text()}.{self.month_lineEdit.text()}.{self.year_lineEdit.text()}"
+        # labell = self.task_name_lineEdit.text()
+            self.db_workk.delete_task(id)
+            quers = self.db_workk.get_day_tasks(f"{self.listWidget.currentItem().text().split()[1]}")
+        #print(quers)
+            self.listWidget.clear()
+            for row in quers:
+                # print(row)
+                values = "   ".join(map(str, row))
+                # print("___".join(row))
+                self.listWidget.addItem(values)
 
     def open_tasks_list(self):
-        print("tasks_list")
+        # print("tasks_list")
         dil = tasks_list_Dialog(self)
         dil.exec()
 
